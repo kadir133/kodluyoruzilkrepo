b28d6c70ab0ffa866cfcc1aa5fb89c28997e8464
kadir133 <37574590+kadir133@users.noreply.github.com>
2022-09-14 09:15:01 +0300

    Create sql_queries.sql

---
+++ sql_queries.sql
@@ -0,0 +1,5 @@
+1 - SELECT title,description FROM film 
+2 - SELECT * FROM film WHERE length>60 And length<75
+3 - SELECT * FROM film WHERE rental_rate=0.99 and (replacement_cost=12.99 OR replacement_cost=28.99)
+4 - SELECT last_name FROM customer where first_name='Mary'
+5 - SELECT * FROM film WHERE length<=50 And not(rental_rate=2.99 or rental_rate=4.99)
