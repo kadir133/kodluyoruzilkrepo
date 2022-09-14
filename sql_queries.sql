6065926d262e8bbf228fbb5de58ff328a352ee0a
kadir133 <37574590+kadir133@users.noreply.github.com>
2022-09-14 09:25:10 +0300

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,5 +1,13 @@
+ÖDEV - 1
+
 1 - SELECT title,description FROM film 
 2 - SELECT * FROM film WHERE length>60 And length<75
 3 - SELECT * FROM film WHERE rental_rate=0.99 and (replacement_cost=12.99 OR replacement_cost=28.99)
 4 - SELECT last_name FROM customer where first_name='Mary'
 5 - SELECT * FROM film WHERE length<=50 And not(rental_rate=2.99 or rental_rate=4.99)
+
+ÖDEV - 2
+
+1 - SELECT * FROM film WHERE replacement_cost between 12.99 And 16.99
+2 - SELECT * FROM actor where first_name IN ('Penelope', 'Nick', 'Ed')
+3 - SELECT * FROM film WHERE rental_rate IN (0.99, 2.99, 4.99) And replacement_cost IN (12.99, 15.99, 28.99)
