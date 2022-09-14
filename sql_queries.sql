0d5dfe5a2c9cefa16d4f09d1e190922dfe8f0539
kadir133 <37574590+kadir133@users.noreply.github.com>
2022-09-14 10:39:24 +0300

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -11,3 +11,10 @@
 1 - SELECT * FROM film WHERE replacement_cost between 12.99 And 16.99
 2 - SELECT * FROM actor where first_name IN ('Penelope', 'Nick', 'Ed')
 3 - SELECT * FROM film WHERE rental_rate IN (0.99, 2.99, 4.99) And replacement_cost IN (12.99, 15.99, 28.99)
+
+ÖDEV - 3
+
+1 - SELECT * FROM country WHERE country LIKE 'A%a'
+2 - SELECT * FROM country WHERE country LIKE '_____%n'
+3 - SELECT * FROM film WHERE title ILIKE '%t%' AND (LENGTH(title)-LENGTH(Replace(REPLACE(title,'T',''),'t','')))>=4
+4 - SELECT * FROM film WHERE length>90 And title Like 'C%' AND rental_rate=2.99
