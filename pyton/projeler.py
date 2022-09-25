e433c7f1b39abce1235635c8768547f0f70a5ab8
kadir133 <37574590+kadir133@users.noreply.github.com>
2022-09-25 21:39:30 +0300

    Update and rename proje.py to projeler.py

--- pyton/projeler.py
+++ pyton/projeler.py
@@ -1,3 +1,5 @@
+1
+
 list = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 newlist = []
 def flatten(x):
@@ -8,3 +10,15 @@ def flatten(x):
             newlist.append(i)
 flatten(list)
 print(newlist)
+
+2
+
+list = [[1, 2], [3, 4], [5, 6, 7]]
+list.reverse()
+
+def reverseList(x):
+    for i in x:
+            i.reverse()
+                
+reverseList(list)
+print(list)
