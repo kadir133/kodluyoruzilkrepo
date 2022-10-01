1c64fe0f76bb63db7ed23e8e76a6d3bf0f0906b7
kadir133 <37574590+kadir133@users.noreply.github.com>
2022-10-02 00:37:04 +0300

    Add files via upload

---
+++ java/java odevler/draw_triangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Kenar uzunluğunu giriniz :");
+    int length = scanner.nextInt();
+
+    int top = (length * 2 + 3) / 2;
+    for (int i = 0; i < length; i++) {
+      System.out.print(repeat(' ', top - i));
+      System.out.println(repeat('*', (i * 2) + 1));
+    }
+  }
+
+  static String repeat(char c, int n) {
+    return new String(new char[n]).replace('\0', c);
+  }
+}
