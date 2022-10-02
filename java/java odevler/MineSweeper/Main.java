30be941360edd6a7a4dd12213e27e641e2a073ab
kadir133 <37574590+kadir133@users.noreply.github.com>
2022-10-02 22:36:04 +0300

    Update Main.java

--- java/java odevler/MineSweeper/Main.java
+++ java/java odevler/MineSweeper/Main.java
@@ -3,13 +3,11 @@ public class Main{
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Mayın tarlası oynuna hoş geldin !\n"+
-        "Matrix boyutunu gir : ");
+        System.out.print("Mayın tarlası oynuna hoş geldin !\nMatrix boyutunu gir : ");
         int x , y ;
         x = scanner.nextInt();
         y = scanner.nextInt();
         MineSweeper mineSweeper = new MineSweeper(x, y);
         mineSweeper.run();
-        mine.run();
     }
 }
