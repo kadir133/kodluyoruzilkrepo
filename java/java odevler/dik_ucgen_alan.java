5ebad9bd780dbd0d2b37b51086d7515d5de765ed
kadir133 <37574590+kadir133@users.noreply.github.com>
2022-10-03 10:07:12 +0300

    Update and rename dik_ucgen.java to dik_ucgen_alan.java

--- java/java odevler/dik_ucgen_alan.java
+++ java/java odevler/dik_ucgen_alan.java
@@ -15,8 +15,6 @@ public class Main {
         System.out.print("3. kenar uzunluğunu giriniz:");
         c=inp.nextInt();
 
-        // kenar3=Math.sqrt((kenar1*kenar1)+(kenar2*kenar2));
-
         var u=(a+b+c)/2;
          System.out.print("Alan :"+ u * (u - a)* (u -b) * (u -c));
          System.out.print("Çevre :"+2*u);
